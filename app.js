97b2a8826429f5ee4f06796cd61db15b980d55da
trmpsanjay <trmpsanjay@gmail.com>
2021-01-13 21:09:28 +0530

    level 3 environment variable implemented

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 
 app.set("view engine","ejs");
@@ -23,8 +25,8 @@ const userSchema =new mongoose.Schema({
 });
 
 //encryption create it before mongoose model
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 const User = mongoose.model("User",userSchema);
 
